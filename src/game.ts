39273c37589cc8159b32347da09d5cd2f0aff128
Remigiusz Najbar <muffespl@gmail.com>
2021-03-31 14:43:16 +0200

    Added validation

--- src/game.ts
+++ src/game.ts
@@ -1,12 +1,12 @@
 import { IsString, IsNumber, IsOptional } from 'class-validator';
 
 export class Game {
-    @IsNumber() @IsOptional()
-    readonly id: number;
+  @IsNumber() @IsOptional()
+  readonly id: number;
 
-    @IsString()
-    readonly name: string;
+  @IsString()
+  readonly name: string;
 
-    @IsString() @IsOptional()
-    readonly password: string;
+  @IsString() @IsOptional()
+  readonly password: string;
 }
--- src/games/games.controller.ts
+++ src/games/games.controller.ts
@@ -1,32 +1,38 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, UseGuards } from '@nestjs/common';
 import { GamesService } from './games.service';
 import { Games } from '../games';
 import { Game } from '../game';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('games')
 export class GamesController {
   constructor(private readonly gamesService: GamesService) { }
 
+  @UseGuards(JwtAuthGuard)
   @Get()
   async index(): Promise<Games> {
     return this.gamesService.findAll();
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   async find(@Param('id') id: number): Promise<Game> {
     return this.gamesService.find(id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   async create(@Body() game: Game) {
     this.gamesService.create(game);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Put()
   async update(@Body() game: Game) {
     this.gamesService.update(game);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   async delete(@Param('id') id: number) {
     this.gamesService.delete(id);

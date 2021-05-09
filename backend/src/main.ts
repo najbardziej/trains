cea000840c991bb76c69a4be9785d8e874de9f2f
Remigiusz Najbar <muffespl@gmail.com>
2021-05-10 01:22:53 +0200

    added compression middleware

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,13 +3,14 @@ import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as compression from 'compression';
 
 dotenv.config()
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-
+  app.use(compression());
   const swaggerConfig = new DocumentBuilder()
     .setTitle('Trains api')
     .setVersion('0.1')

418db52d71a1c0d76ae3c2f354116eff0fe8d331
Remigiusz Najbar <muffespl@gmail.com>
2022-01-06 23:36:10 +0100

    Updated nestjs, dependencies

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,4 +1,5 @@
-import { HttpModule, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
 import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
 import { PassportModule } from '@nestjs/passport';

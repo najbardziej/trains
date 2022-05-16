b2e0e65d1b7d13c387bc08897ce1bf0de577bfe5
Remigiusz Najbar <muffespl@gmail.com>
2022-05-17 01:24:01 +0200

    Added map elements scalling!!!

--- frontend/src/app/game/game-map/game-map.component.ts
+++ frontend/src/app/game/game-map/game-map.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { contain } from 'intrinsic-scale';
+
+const MAP_HEIGHT = 921.633;
+const MAP_WIDTH = 1408;
 
 @Component({
   selector: 'game-map',
@@ -10,23 +14,31 @@ export class GameMapComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.rescaleMap();
+  }
+
+  rescaleMap() {
+    const gameMapOverlay = document.querySelector('.game-map__overlay') as HTMLElement;
+    const gameMap = document.querySelector('game-map');
+    const gameMapWidth = gameMap!.getBoundingClientRect().width;
+    const gameMapHeight = gameMap!.getBoundingClientRect().height;
+    console.log({gameMapWidth, gameMapHeight}, contain(gameMapWidth, gameMapHeight, MAP_WIDTH, MAP_HEIGHT));
+    let { width, height, x, y } = contain(gameMapWidth, gameMapHeight, MAP_WIDTH, MAP_HEIGHT);
+    const scale = width / MAP_WIDTH;
+    gameMapOverlay.style.transform = `scale(${scale})`;
   }
 
   createTrain(event: any) {
     const gameMap = document.querySelector('game-map');
+    const gameMapOverlay = document.querySelector('.game-map__overlay');
     const mapWidth = gameMap!.getBoundingClientRect().width;
     const mapHeight = gameMap!.getBoundingClientRect().height;
-    const train = document.createElement('train');
-    train.style.position = "absolute";
+    const train = document.createElement('div');
+    train.classList.add("train");
     train.style.top = `${(event.clientY / mapHeight) * 100}%`;
     train.style.right = `${(gameMap!.getBoundingClientRect().right - event.clientX) / mapWidth * 100}%`;
-    train.style.width = "45px";
-    train.style.height = "15px";
-    train.style.background = "gray";
-    train.style.boxShadow = "2px 2px 7px black, inset 2px 2px 5px rgba(255, 255, 255, 0.7)";
-    train.style.border = "2px solid darkgray"
-    train.style.cursor = "pointer"
-    gameMap!.appendChild(train);
+
+    gameMapOverlay!.appendChild(train);
   }
 
 }

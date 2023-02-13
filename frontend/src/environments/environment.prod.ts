6e21f04d6f9629b422bf13aacf3c26e5844fbf90
Remigiusz Najbar <muffespl@gmail.com>
2023-02-13 22:13:30 +0100

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://trains.najbardziej.net',
+  apiUrl: 'https://trains-backend.najbardziej.net',
   googleClientID: "780124251729-9cdpmrc7edqpnq0gknnsoftth0fmetbj.apps.googleusercontent.com",
 };
